6f39619191d53734b0070c3238a2b8341a5cf837
Nata <Nata@mail.ru>
2020-06-20 09:34:23 +0200

    add test

---
+++ src/test/java/ua/com/foxminded/racers/RacersListTest.java
@@ -0,0 +1,14 @@
+package ua.com.foxminded.racers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class RacersListTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
