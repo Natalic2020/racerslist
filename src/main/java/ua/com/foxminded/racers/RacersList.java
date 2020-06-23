53dced9e4c4fbe66a62592f504e63986b2962905
Nata <Nata@mail.ru>
2020-06-23 17:50:41 +0200

    small format

--- src/main/java/ua/com/foxminded/racers/RacersList.java
+++ src/main/java/ua/com/foxminded/racers/RacersList.java
@@ -20,7 +20,7 @@ import java.util.stream.Stream;
 public class RacersList {
 
 	private static final String PATTERN_DATA_TIME = "yyyy-MM-dd HH:mm:ss.SSS";
-	
+
 	public Stream<String> formRacersList(String fileStart, String fileEnd, String fileAbbreviations) {
 		if (!checkFile(fileStart)) {
 			return Stream.empty();
@@ -42,56 +42,56 @@ public class RacersList {
 		String fileAbbreviations = reseivePath(fileAbbreviationsName);
 
 		List<RacerData> racerDataList;
-		try ( Stream<String> fileInStream = Files.lines(Paths.get(fileAbbreviations))){
+		try (Stream<String> fileInStream = Files.lines(Paths.get(fileAbbreviations))) {
 			racerDataList = fileInStream.map(temp -> new RacerData(temp)).collect(Collectors.toList());
 		} catch (IOException e) {
-			racerDataList = new ArrayList<>(); 
+			racerDataList = new ArrayList<>();
 			e.printStackTrace();
 		}
-		
+
 		Map<String, String> mapStart = convertFileToMap(fileStart);
 		Map<String, String> mapEnd = convertFileToMap(fileEnd);
-		
+
 		Map<String, String> mapTime = Stream.of(mapStart, mapEnd).flatMap(m -> m.entrySet().stream())
-				.collect(Collectors.toMap(Entry::getKey, Entry::getValue, (o1,
-						o2) -> o1 + o2));
-		
+				.collect(Collectors.toMap(Entry::getKey, Entry::getValue, (o1, o2) -> o1 + o2));
+
 		List<RacerData> racerDataListSorted = racerDataList.stream().filter(s -> !mapTime.get(s.getAbbr()).isEmpty())
-			.map(s->{s.setRacerTime(reseiveTime(mapTime.get(s.getAbbr())));
-				return s;})
-			.sorted(Comparator.comparing(RacerData::getRacerTime)).collect(Collectors.toList());
-		
+				.map(s -> {
+					s.setRacerTime(reseiveTime(mapTime.get(s.getAbbr())));
+					return s;
+				}).sorted(Comparator.comparing(RacerData::getRacerTime)).collect(Collectors.toList());
+
 		return racerDataListSorted;
 	}
-	
+
 	private Duration reseiveTime(String time) {
-		String timeStart = time.substring(0,23);
+		String timeStart = time.substring(0, 23);
 		String timeEnd = time.substring(23);
 		return Duration.between(convertStringToLocalDT(timeStart), convertStringToLocalDT(timeEnd));
 	}
-	
+
 	private LocalDateTime convertStringToLocalDT(String text) {
 		return LocalDateTime.parse(text.replace('_', ' '), DateTimeFormatter.ofPattern(PATTERN_DATA_TIME));
 	}
-	
+
 	private Map<String, String> convertFileToMap(String file) {
 		Map<String, String> mapAbbreviations = new HashMap<>();
-		try ( Stream<String> fileInStream = Files.lines(Paths.get(file))){
-			mapAbbreviations = fileInStream
-					.collect(Collectors.toMap(i -> i.substring(0, 3), i -> i.substring(3)));
+		try (Stream<String> fileInStream = Files.lines(Paths.get(file))) {
+			mapAbbreviations = fileInStream.collect(Collectors.toMap(i -> i.substring(0, 3), i -> i.substring(3)));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		return mapAbbreviations;
 	}
-	
+
 	private String reseivePath(String file) {
 		return getClass().getClassLoader().getResource(file).getPath().substring(1);
 	}
 
 	private Stream<String> formOutputListRacers(List<RacerData> racerDataList) {
 		Stream<String> first15 = formOutputListRacers(racerDataList, 1, 15);
-		Stream<String> separator = Stream.of(String.format("%s%n","*----------------------------------------------------------------"));
+		Stream<String> separator = Stream
+				.of(String.format("%s%n", "*----------------------------------------------------------------"));
 		Stream<String> last = formOutputListRacers(racerDataList, 16, racerDataList.size());
 		return Stream.concat(Stream.concat(first15, separator), last);
 	}
@@ -110,7 +110,7 @@ public class RacersList {
 		if (fileName.isEmpty()) {
 			throw new IllegalArgumentException("Null parameters are not allowed");
 		}
-		File f = new File(reseivePath(fileName)); 
+		File f = new File(reseivePath(fileName));
 		if (f.isFile()) {
 			return true;
 		}
